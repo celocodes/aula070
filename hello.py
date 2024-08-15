4f1838d1421d6ec79c1ebe8f2a839ff7f607d21b
cel01_ <102827591+celocodes@users.noreply.github.com>
2024-08-15 20:55:39 -0300

    aaaaa

--- hello.py
+++ hello.py
@@ -7,6 +7,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import requests
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
